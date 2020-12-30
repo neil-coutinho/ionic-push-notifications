92bd07369d5ed540f2abca8394db097e34233007
Neil Coutinho <njcoutinho@gmail.com>
2020-12-30 11:14:43 +0530

    Firebase config via @angular/fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyChKobqlHYG9U-yjBIPs2fTdm3TT_jE7-c",
+    authDomain: "push-notifications-a7993.firebaseapp.com",
+    projectId: "push-notifications-a7993",
+    storageBucket: "push-notifications-a7993.appspot.com",
+    messagingSenderId: "437327802529",
+    appId: "1:437327802529:web:fe3ed9ac4eb2e06df700e0"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyChKobqlHYG9U-yjBIPs2fTdm3TT_jE7-c",
+    authDomain: "push-notifications-a7993.firebaseapp.com",
+    projectId: "push-notifications-a7993",
+    storageBucket: "push-notifications-a7993.appspot.com",
+    messagingSenderId: "437327802529",
+    appId: "1:437327802529:web:fe3ed9ac4eb2e06df700e0"
+  }
 };
 
 /*
