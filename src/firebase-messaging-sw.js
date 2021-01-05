51cf059e7ff2a7fc458a9c880d78fe34ac2a3008
Neil Coutinho <njcoutinho@gmail.com>
2021-01-05 15:36:00 +0530

    firebase-messaging-sw.js config

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,8 +1,8 @@
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here, other Firebase libraries
 // are not available in the service worker.
-importScripts('https://www.gstatic.com/firebasejs/9.1.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/9.1.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/8.2.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.2.1/firebase-messaging.js');
 
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
@@ -11,8 +11,13 @@ importScripts('https://www.gstatic.com/firebasejs/9.1.0/firebase-messaging.js');
 //const { messagingSenderId } = environment.firebase;
 
 firebase.initializeApp({
-    messagingSenderId: 437327802529
-});
+    apiKey: "AIzaSyChKobqlHYG9U-yjBIPs2fTdm3TT_jE7-c",
+    authDomain: "push-notifications-a7993.firebaseapp.com",
+    projectId: "push-notifications-a7993",
+    storageBucket: "push-notifications-a7993.appspot.com",
+    messagingSenderId: "437327802529",
+    appId: "1:437327802529:web:fe3ed9ac4eb2e06df700e0"
+  });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
